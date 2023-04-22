5a704f5aa136eee2ae4f3ce4c6575ab212154431
Guilherme Tramontano <tramontano.guilherme@yahoo.com.br>
2023-04-22 07:05:36 +0000

    feat: creating connection with websocket

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,17 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+import { io } from 'socket.io-client';
 import whatsappBoundary from './Client';
+const socket = io('http://gramont.ddns.net:4521');
 
 whatsappBoundary().initialize();
+
+socket.on('connect', () => {
+	console.log(socket.id);
+	socket.emit('message', 'socorro');
+});
+
+socket.on('response', socket => {
+	console.log(socket);
+});
