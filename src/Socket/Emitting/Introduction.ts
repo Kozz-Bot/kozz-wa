c40db411c5a78add2f1572502a9ccdf9b390dee5
Guilherme Tramontano <tramontano.guilherme@yahoo.com.br>
2023-05-23 21:46:27 +0000

    feat: signing payload for introduction

--- src/Socket/Emitting/Introduction.ts
+++ src/Socket/Emitting/Introduction.ts
@@ -1,13 +1,14 @@
-import { type BoundaryIntroduction } from 'kozz-types';
+import { type BoundaryIntroduction, SignaturelessPayload } from 'kozz-types';
 import { Socket } from 'socket.io-client';
+import { signPayload } from 'src/util/PayloadSign';
 
 export const registerIntroductionSocketEvent = (socket: Socket) => {
-	const introduction: BoundaryIntroduction = {
+	const introduction: SignaturelessPayload<BoundaryIntroduction> = {
 		OS: process.platform,
 		platform: 'WA',
 		role: 'boundary',
 		timestamp: new Date().getTime(),
 		id: process.env.BOUNDARY_ID,
 	};
-	socket.emit('introduction', introduction);
+	socket.emit('introduction', signPayload(introduction));
 };
