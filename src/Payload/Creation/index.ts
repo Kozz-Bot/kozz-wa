b91624a2076d285327c80030b6f877bee09ba113
Guilherme Tramontano <tramontano.guilherme@yahoo.com.br>
2023-05-25 18:18:57 +0000

    chore: removing debug code

--- src/Payload/Creation/index.ts
+++ src/Payload/Creation/index.ts
@@ -3,11 +3,6 @@ import { Message, MessageMedia } from 'whatsapp-web.js';
 import { createMessageReveivedPayload } from './MessageReceived';
 
 export const onMessageReceived = (socket: Socket) => async (message: Message) => {
-	if (message.body === '!ping') {
-		message.reply('pong');
-		return;
-	}
-
 	const payload = await createMessageReveivedPayload(message);
 	socket.emit('message', payload);
 };
