cb7d8f6d4bdbb14e8a9fed47395753c15d035c76
Guilherme Tramontano <tramontano.guilherme@yahoo.com.br>
2023-09-18 16:43:02 +0000

    feat: removing necessity of authenticate with .env file and creating .env.example

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ dotenv.config();
 import { io } from 'socket.io-client';
 import whatsappBoundary from './Client';
 import { registerSocketHandlers } from './Socket';
-const socket = io(`${process.env.GATEWAY_URL}`);
+const socket = io(`${process.env.GATEWAY_URL}` || '127.0.0.1:4521');
 
 const wppClient = whatsappBoundary(socket);
 wppClient.initialize();
